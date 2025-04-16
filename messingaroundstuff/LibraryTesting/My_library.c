1d283c46021b8056d8519311287d729c1ae7a627
spectacularalphanumerics <theonicolaou1@protonmail.com>
2025-04-16 13:24:04 +0300

    Update My_library.c

--- messingaroundstuff/LibraryTesting/My_library.c
+++ messingaroundstuff/LibraryTesting/My_library.c
@@ -3,21 +3,22 @@
 #include <math.h>
 #define PI 3.142
 
-void sayHello(){
+void sayHello() {
 	printf("Hello world!\n");
-)
-
-int add(int a, int b) {
-	int result;
-	result = a+b;
-	return result;
+	)
 }
 
-double area(double radius) {
-	double areaofcircle = PI * pow(radius, 2);
-	return areaofcircle;
-}
+	int add(int a, int b){
+		int result;
+		result = a+b;
+		return result;
+	}
 
-int length(char *x) {
-	return strlen(x);
-}
+	double area(double radius){
+		double areaofcircle = PI * pow(radius, 2);
+		return areaofcircle;
+	}
+
+	int length(char *x){
+		return strlen(x);
+	}
