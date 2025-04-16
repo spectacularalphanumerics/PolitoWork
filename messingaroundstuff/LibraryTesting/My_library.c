b811ae4a725144bd91bd1f69a7822bbe99306b30
spectacularalphanumerics <theonicolaou1@protonmail.com>
2025-04-16 13:22:38 +0300

    Create My_library.c

---
+++ messingaroundstuff/LibraryTesting/My_library.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#define PI 3.142
+
+void sayHello(){
+	printf("Hello world!\n");
+)
+
+int add(int a, int b) {
+	int result;
+	result = a+b;
+	return result;
+}
+
+double area(double radius) {
+	double areaofcircle = PI * pow(radius, 2);
+	return areaofcircle;
+}
+
+int length(char *x) {
+	return strlen(x);
+}
